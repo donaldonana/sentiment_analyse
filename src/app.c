e859bb6cd5bcdb70e401c57507c6edb2d3c6f410
donaldonana <onanadonald@gmail.com>
2022-04-28 11:48:27 +0200

    para et seq

--- src/app.c
+++ src/app.c
@@ -853,14 +853,83 @@ for (int i = 0; i < phrases[0].nm; i++)
       free(target);
       //free(phrases);
   }
-
+// =========================================================================================
+// =========================================================================================
   else {
 
-    printf (" ----------end-------");
-  }
-  
-  
+      double **last_h;
+      double loss;
+      double *dl_dy = malloc(sizeof(double)*rnn->output_size);
+      int id;
+      int *index = malloc(sizeof(int)*np);
 
+      clock_t begin = clock();
+      printf("\n\n\tTRAINING PHASE IN PROCESS\n\n");
+      for (int i = 0; i < 1000; i++)
+      {
+        loss = 0;
+        randomize(index, np);
+
+        for (int j = 0; j < np; j++)
+        {
+        //forward
+            id = j;
+          // id = j;
+            //int j = rand() % (np);
+            //printf("\n%d\n",j);
+
+            last_h = forward(rnn,  phrases[id].w2vec, phrases[id].nm);
+            //printf("\n--->%d\n", j);
+            //loss = loss-log(rnn->y[target[id]]);
+            //printf("log error : %lf", loss);
+            //# Build dL/dy
+            copy_vect(dl_dy, rnn->y, rnn->output_size);
+            dl_dy[target[id]] = dl_dy[target[id]] - 1 ;
+          
+            //backforward
+            //printf("\n------%d--b----\n", i);
+
+            backforward(rnn, dl_dy, last_h, phrases[id].nm);
+
+          
+          //for (int k = 0; k < rnn->output_size; k++)
+          //{
+            //printf("\n %d : %lf", k, rnn->y[k]);
+          //}
+        }
+          
+          
+        //if (1)
+        if (i % 100 == 99)
+        {
+          printf("\n-------------------EPOCH %d----------------\n", i+1);
+          //printf("erreur et prediction : \n");
+          forward(rnn, phrases[1].w2vec, phrases[1].nm);
+          loss = (-1)*log(rnn->y[target[1]]);
+          printf("\n log error : %lf \n", loss);
+          /*for (int k = 0; k < rnn->output_size; k++)
+          {
+            printf("\n %d : %lf", k, rnn->y[k]);
+          }*/
+          printf("\n");
+
+          //for (int n = 0; n < rnn->output_size; n++)
+          //{
+              //printf("%lf \n", rnn->by[n]);
+            //}
+        }
+
+      }
+
+      clock_t end = clock();
+      double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+      printf("\n\t FINAL ERROR : %lf \n", loss);
+      printf("\n\t FULL TRAIN TIME: %lf s\n", time_spent);
+
+    }
+  
+  
   return 0;
 }
 
