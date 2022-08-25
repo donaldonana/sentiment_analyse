45c95c532430050eb43814837d3cd474e134d7ce
onana <onanadonald@gmail.com>
2022-08-25 20:26:59 +0100

    messages

--- python/sentiments_analyse/Pre_Processing.py
+++ python/sentiments_analyse/Pre_Processing.py
@@ -6,8 +6,12 @@ from data_set import train_data, test_data
 
 items = list(train_data.items())
 
-with open('../../target.txt', 'w') as mon_fichier:
-  for x, y in items:
-    target = int(y)
-    print("\n",target,"\n")
-    mon_fichier.write(str(target)+"\n")
+with open('../../sentiments.csv', 'w') as mon_fichier:
+
+	mon_fichier.write("text,label\n")
+	for x, y in items:
+		target = int(y)
+		# print("\n",target,"\n")
+		# mon_fichier.write(str(target)+"\n")
+		print("\n---")
+		mon_fichier.write(str(x)+","+str(target)+"\n")
